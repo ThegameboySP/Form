a5212973ab9e24bc6eb45ad45abac1a276038bde
Thegameboy <delfinocorner@gmail.com>
2021-10-06 12:02:19 -0400

    Fix Schema from callback to table

---
+++ src/Components/Form/Types.lua
@@ -0,0 +1,19 @@
+local t = require(script.Parent.Parent.Modules.t)
+local NetworkMode = require(script.Parent.Parent.Shared.NetworkMode)
+
+return {
+	ComponentDefinition = t.interface({
+		CheckRef = t.optional(t.callback);
+		RequiredEmbedded = t.optional(t.table);
+		Defaults = t.optional(t.table);
+		Schema = t.optional(t.table);
+
+		NetworkMode = t.optional(t.valueOf(NetworkMode));
+		ClassName = t.string;
+
+		-- For component-specific initalization and accessing external things.
+		Init = t.optional(t.callback);
+		-- For firing events and setting into motion internal processes.
+		Start = t.optional(t.callback);
+	})
+}
